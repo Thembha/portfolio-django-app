528e0b04d5ba30f9551a704cd243ac66d488591f
Thembha <thembhatro@gmail.com>
2022-12-07 17:51:45 +0200

    profile update template added.

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,10 @@ from .models import Profile
 
 
 class UpdateUserForm(forms.ModelForm):
-    username = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
+    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
 
     class Meta:
         model = User
@@ -16,10 +16,10 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
-    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
     home_address = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}))
-    location_lat = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    location_lon = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    location_lat = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
+    location_lon = forms.DecimalField(widget=forms.TextInput(attrs={'class': 'form-control textbox'}))
     
     class Meta:
 
