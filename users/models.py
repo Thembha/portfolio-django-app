51c786c8466baadd5f010115589b7fcc361985d3
Thembha <thembhatro@gmail.com>
2022-12-07 05:27:09 +0200

    models: Profile: location allow null

--- users/models.py
+++ users/models.py
@@ -8,11 +8,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     home_address = models.TextField(max_length=500, null=False, blank=False)
     phone_regex = RegexValidator(
-        regex=r'^\+\d{8,15}$', message="Only 15 digits are allowed and the format shoulkd be: '+11111111111'."
+        regex=r'^\+\d{8,15}$', message="Only 15 digits are allowed and the format should be: '+11111111111'."
     )
     phone_number = models.CharField(validators=[phone_regex], max_length=16, null=False, blank=False)
-    location_lat = models.DecimalField(max_digits=9, decimal_places=6)
-    location_lon = models.DecimalField(max_digits=9, decimal_places=6)
+    location_lat = models.DecimalField(max_digits=9, decimal_places=6, null=True)
+    location_lon = models.DecimalField(max_digits=9, decimal_places=6, null=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
