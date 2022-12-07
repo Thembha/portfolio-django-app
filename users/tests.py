1749271d7aacec77807fe02d3e0963867108c23b
Thembha <thembhatro@gmail.com>
2022-12-07 05:23:31 +0200

    adding profile model test

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+class ProfileModelTests(TestCase):                                                                                        
+    @classmethod                                                                                                          
+    def setUpTestData(cls):                                                                                               
+        User.objects.create()                                                                                                                                                                                   
+
+    def test_profile(self):                                                                                    
+        profile = User.objects.last().profile
