c74dbf2fc84a1c14b58982d227481e5e6c265fe9
Thembha <thembhatro@gmail.com>
2022-12-08 15:38:19 +0200

    login and logout complete

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
-from . views import index, edit, map
+from django.contrib.auth import views as auth_views
+from . views import index, index_from_map, edit, map
 
 urlpatterns = [
     path('', index, name='index'),
+    path('profile/<user_id>/', index_from_map, name='index_from_map'),
     path('edit/', edit, name='edit'),
     path('map/', map, name='map'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True, template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
